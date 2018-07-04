770ac01c26a5d8ff2485ac66ebc68639df901279
jbdsfks <838279926@qq.com>
2018-07-04 16:51:12 +0800

    test

--- controllers/fconn.js
+++ controllers/fconn.js
@@ -1,20 +1,22 @@
-
+/**
+ * 根据用户名创建Fabric连接对象
+ * @param {*} username 
+ * 如果正常就返回连接对象
+ * 
+ */
 async function FConnect(username){
     var fc = null;
     var fs = require('fs');
-// var args = process.argv.splice(2);
     var file = '../fabcar/Fabks.json';
     if (file == null) {
         console.log('需要指定一个连接json文件,如Fabcar.json,或Fabks.json，TLSConn.json');
         return;
     }
-//console.log(file);
     var baseconn = JSON.parse(fs.readFileSync(file));
-
     try {
         var FabKSinvoke = require('../../fabcar/FabKSinvoke');
         fc = new FabKSinvoke();
-        await fc.connect(baseconn, username); //args[1]第2个参数可选，一个用户名
+        await fc.connect(baseconn, username);
         // fc.currentuser();
         return fc
         // req.session.Fab = fc;
